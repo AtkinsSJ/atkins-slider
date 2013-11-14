102c44208d6853dcbea9467a4c91842c138ddb2f
Sam Atkins <github@samatkins.co.uk>
2013-11-14 13:32:49 +0000

    Created slider dots and caption div.

--- atkins-slider.js
+++ atkins-slider.js
@@ -2,9 +2,29 @@
  * atkins-slider.js
  */
 jQuery(document).ready(function($) {
-	var createGallery = function($gallery) {
-		
+	var createGallery = function(index, gallery) {
+		var $gallery = $(gallery);
+		console.log($gallery);
+
+		var $thumbnails = $gallery.children('.gallery-item');
+
+		// Create caption area
+		var $caption = $('<div>').addClass('slider-caption')
+							.appendTo($gallery);
+			$caption.text("Slider Caption");
+
+		// Create dots
+		var $dotList = $('<ol>').addClass('slider-dots')
+							.appendTo($gallery);
+		for (var i=0; i<$thumbnails.length; i++) {
+			$dotList.append( $('<li class="slider-dot" data-index="'+i+'">') );
+		}
+
+		// Resize thumbnails
+
+		// Hide caption elements
+		$gallery.find('dd').hide();
 	};
-	
+
 	$('.gallery').each(createGallery);
 });
